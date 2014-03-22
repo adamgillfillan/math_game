9f341921773cff0cbe33254aa87ba1632a8eeff7
Adam Gillfillan <agillfi@ncsu.edu>
2014-03-22 12:50:49 -0400

    added destroy and move scripts

---
+++ Assets/Scripts/DestroyScript.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Update () {
+	if (Input.GetMouseButtonDown(0))
+     {
+       Debug.Log(Camera.main.ScreenPointToRay(Input.mousePosition));
+       //make sure you have a camera in the scene tagged as 'MainCamera'
+       var hitm : RaycastHit2D = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+       if(hitm.collider != null)
+       {
+           Debug.Log ("Target Position: " + hitm.collider.gameObject.transform.position);
+         Destroy (hitm.collider.gameObject);
+       }
+     }
+}
