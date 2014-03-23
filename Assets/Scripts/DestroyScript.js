788b67fa24c01efb20a1ddbddf528336ef5f8dfc
Adam Gillfillan <agillfi@ncsu.edu>
2014-03-22 20:00:41 -0400

    score updates work now

--- Assets/Scripts/DestroyScript.js
+++ Assets/Scripts/DestroyScript.js
@@ -4,8 +4,6 @@
 var answer_int : int;
 answer_int = get_answer();
 
-//var math_answer : int;
-//math_answer = get_math_problem_answer();
 function get_answer(){
 	var answer_object : GameObject;
 	var my_answer : int;
@@ -17,24 +15,11 @@ function get_answer(){
 	return my_answer;
 }
 
-//function get_math_problem_answer() {
-//	//var math_answer_object : GameObject;
-//	var math_answer : int;
-//	var math_answer_object_text : String;
-//	
-//	var my_object : TextMesh;
-//	my_object = gameObject.GetComponentInChildren(TextMesh);
-//	math_answer_object_text = my_object.text;
-//	var split : String[] = math_answer_object_text.Split("+"[0]);
-//	//var value1 : int = parseInt(split[0]);
-//	//var value2 : int = parseInt(split[1]);
-//	math_answer = parseInt(split[0]) + parseInt(split[1]); //+ value2;// + parseInt(split[1]);
-//	//Debug.Log("Math answer: " + math_answer); //+ value2;// + parseInt(split[1]);
-//	return math_answer;
-//}
-
-//var returnAnswer : ReturnAnswer;
-//Debug.Log("ASDF " + other.get_math_problem_answer());
+function updateScore (){
+	var score = GameObject.FindWithTag("Score").GetComponent(ScoreManager);
+	var score_modifier: int = 1;
+	score.add_score(score_modifier);
+}
 function Update () {
 	if (Input.GetMouseButtonDown(0))
      {
@@ -46,12 +31,10 @@ function Update () {
        {
        	 var splitter : String[] = hitm.collider.gameObject.GetComponentInChildren(TextMesh).text.Split("+"[0]);
        	 var my_answer : int = parseInt(splitter[0]) + parseInt(splitter[1]);
-       	 //Debug.Log(hitm.collider.gameObject.GetComponentInChildren(TextMesh).text);//.GetComponent
-       	 //Debug.Log("TRUE ANSWER: " + my_answer);
-       	 //Debug.Log("Answer: " + get_math_problem_answer());
+       	 
          if(answer_int == my_answer){
-         //Debug.Log ("Target Position: " + hitm.collider.gameObject.transform.position);
          	Destroy (hitm.collider.gameObject);
+         	updateScore();
          }
        }
      }
---
+++ Assets/Scripts/ScoreManager.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+
+var score : int;
+//var score_text_object : GameObject;
+//var score_text : String;
+//score_text_object = GameObject.FindWithTag("Score");
+//score_text = score_text_object.GetComponent(TextMesh).text;
+
+function Start () {
+	GetComponent(TextMesh).text = "Score: " + 0;
+}
+
+function add_score(value : int){
+	score += value;
+	GetComponent(TextMesh).text = "Score: " + score;
+}
