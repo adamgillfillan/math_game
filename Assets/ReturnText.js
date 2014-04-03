88c662239cb698dba459c8b6f14816ea8ed907b3
Adam Gillfillan <agillfi@ncsu.edu>
2014-04-03 14:23:48 -0400

    updated game over script and added foundation for summary text

---
+++ Assets/ReturnText.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+function return_text(){
+	var my_text : String = GetComponent(TextMesh).text;
+	return my_text;
+	//GetComponent(TextMesh).text = "Hello";
+}
---
+++ Assets/Scripts/GameOverScript.js
@@ -0,0 +1,29 @@
+﻿
+function OnGUI()
+{
+	var buttonWidth = 120;
+	var buttonHeight = 60;
+	
+	if (GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2) -60,(2.8 * Screen.height / 3) - (buttonHeight / 2),buttonWidth,buttonHeight),"Play Again!"))
+	{
+		// Reload the level
+		Application.LoadLevel("Scene1");
+	}
+	
+	if (GUI.Button(new Rect(Screen.width / 2 - (buttonWidth / 2) + buttonWidth -30,(2.8 * Screen.height / 3) - (buttonHeight / 2),buttonWidth,buttonHeight),"Back to menu"))
+	{
+		// Reload the level
+		Application.LoadLevel("Menu");
+	}
+}
+
+function get_summary_text(){
+	var my_summary_text : TextMesh;
+	my_summary_text = GameObject.FindWithTag("Summary").GetComponent(TextMesh);
+	return my_summary_text;
+}
+
+function Start(){
+	var summary_text : TextMesh = get_summary_text();
+	summary_text.text = "Hello";
+}
